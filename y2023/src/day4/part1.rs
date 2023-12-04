f09e6e6dc95ad87162ee2aba0647fdc8fcaf1638
RoboRobert <nathan.estep@gmail.com>
2023-12-04 01:01:24 -0600

    makes string parsing for day 4 better

--- y2023/src/day4/part1.rs
+++ y2023/src/day4/part1.rs
@@ -13,34 +13,30 @@ fn main() {
         .expect("File read properly!");
 
     for current in contents.lines() {
-        //Splits the lines into winning numbers and held numbers.
-        let line_vec: Vec<&str> = current.split(['|']).collect();
-
         //This stores all the winning numbers in a u32 vector
-        let winning_vec_str: Vec<&str> = line_vec[1].split([' ']).collect();
+        let winning_vec_str: Vec<&str> =  current.split("|").collect::<Vec<&str>>()[1].split_whitespace().collect();
         let mut winning_vec: Vec<u32> = Vec::new();
-
         for winner in winning_vec_str {
-            let current_winner = winner.parse::<i32>().unwrap_or(-1);
-            if current_winner != -1 {
-                winning_vec.push(current_winner as u32);
-            }   
+            winning_vec.push(winner.parse::<u32>().unwrap());
+                
         }
 
         //This stores all the held numbers in a u32 vector
-        let held_vec_str: Vec<&str> = line_vec[0].split([' ']).collect();
+        let held_vec_str: Vec<&str> = current.split("|").collect::<Vec<&str>>()[0].split(":").collect::<Vec<&str>>()[1].split_whitespace().collect();
         let mut held_vec: Vec<u32> = Vec::new();
         for held in held_vec_str {
-            let held_value = held.parse::<i32>().unwrap_or(-1);
-            if held_value != -1 {
-                held_vec.push(held_value as u32);
-            }   
+            held_vec.push(held.parse::<u32>().unwrap());
         }
-        
-        //Sorts the vectors for easy searching later
-        winning_vec.sort();
-        held_vec.sort();
-        
+
+        let mut matched_ints = 0;
+        //Then, searches the winning numbers for every number in the held vector
+        for current_search in &held_vec {
+            //If the winning vector contains the current value, 
+            if winning_vec.contains(&current_search) {
+                matched_ints += 1;
+            }
+        }
+
         let mut worth = 0;
         //Then, searches the winning numbers for every number in the held vector
         for current_search in held_vec {
--- y2023/src/day4/part2.rs
+++ y2023/src/day4/part2.rs
@@ -1,47 +1,35 @@
 use std::{fs, any};
 
 fn main() {
-    //Value that holds the total sum
-    let mut num_cards = 0;
-
     let mut num_card_type: Vec<u32> = Vec::new();
     let mut num_matches: Vec<u32> = Vec::new();
 
-    let file_path = String::from("input.txt");
+    let file_path = String::from("example.txt");
 
     println!("In file {file_path}");
 
     //Read the file into a string
     let contents = fs::read_to_string(file_path)
-        .expect("File read properly!");
+        .expect("File not read properly!");
 
     for current in contents.lines() {
-        num_cards += 1;
         num_card_type.push(1);
-        //Splits the lines into winning numbers and held numbers.
-        let line_vec: Vec<&str> = current.split(['|']).collect();
 
         //This stores all the winning numbers in a u32 vector
-        let winning_vec_str: Vec<&str> = line_vec[1].split([' ']).collect();
+        let winning_vec_str: Vec<&str> =  current.split("|").collect::<Vec<&str>>()[1].split_whitespace().collect();
         let mut winning_vec: Vec<u32> = Vec::new();
-
         for winner in winning_vec_str {
-            let current_winner = winner.parse::<i32>().unwrap_or(-1);
-            if current_winner != -1 {
-                winning_vec.push(current_winner as u32);
-            }   
+            winning_vec.push(winner.parse::<u32>().unwrap());
+                
         }
 
         //This stores all the held numbers in a u32 vector
-        let held_vec_str: Vec<&str> = line_vec[0].split([' ']).collect();
+        let held_vec_str: Vec<&str> = current.split("|").collect::<Vec<&str>>()[0].split(":").collect::<Vec<&str>>()[1].split_whitespace().collect();
         let mut held_vec: Vec<u32> = Vec::new();
         for held in held_vec_str {
-            let held_value = held.parse::<i32>().unwrap_or(-1);
-            if held_value != -1 {
-                held_vec.push(held_value as u32);
-            }   
+            held_vec.push(held.parse::<u32>().unwrap());
         }
-        
+
         let mut matched_ints = 0;
         //Then, searches the winning numbers for every number in the held vector
         for current_search in held_vec {
@@ -51,14 +39,18 @@ fn main() {
             }
         }
 
+        //Pushes the number of matches for each card type
         num_matches.push(matched_ints);
     }
 
     let mut j = 0;
     let len = num_card_type.len();
+    //Iterates through the vector of the possessed number of each card type
     for iterator in 0..len {
+        //j is used to iterate through a slice of num_card_type
         j = iterator+1;
 
+        //Adds to the
         while j <= iterator + num_matches[iterator] as usize {
             if j > len {
                 break;
@@ -70,7 +62,8 @@ fn main() {
         }
     }
 
-    num_cards = 0;
+        //Value that holds the total sum
+        let mut num_cards = 0;
     for num_current in num_card_type {
         num_cards += num_current;
     }
