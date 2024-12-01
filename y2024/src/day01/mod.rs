71e265dc9196eb1f472359c588a207f0fa947d6d
RoboRobert <nathan.estep@gmail.com>
2024-12-01 00:04:44 -0600

    day 01 of 2024 advent!

--- y2024/src/day01/mod.rs
+++ y2024/src/day01/mod.rs
@@ -1,8 +1,32 @@
 pub fn puzzle1(input: &str) -> u32 {
-    54;
+    let mut left: Vec<u32> = input.lines().map(|line| {line.split_whitespace().nth(0).unwrap().parse::<u32>().unwrap()}).collect();
+    let mut right: Vec<u32> = input.lines().map(|line| {line.split_whitespace().nth(1).unwrap().parse::<u32>().unwrap()}).collect();
+
+    left.sort();
+    right.sort();
+
+    let mut sum: u32 = 0;
+
+    for i in 0..left.len() {
+        sum += left[i].abs_diff(right[i])
+    }
+
+    return sum;
 }
 
 
-pub fn puzzle2(input: &str) -> u32 {
-    52;
+pub fn puzzle2(input: &str) -> u64 {
+    let mut left: Vec<u64> = input.lines().map(|line| {line.split_whitespace().nth(0).unwrap().parse::<u64>().unwrap()}).collect();
+    let mut right: Vec<u64> = input.lines().map(|line| {line.split_whitespace().nth(1).unwrap().parse::<u64>().unwrap()}).collect();
+
+    left.sort();
+    right.sort();
+
+    let mut sum: u64 = 0;
+    
+    for ele in left {
+        sum += (right.iter().filter(|&element| {*element == ele}).count() as u64)*ele;
+    }
+
+    return sum;
 }
