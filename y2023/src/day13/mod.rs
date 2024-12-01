bbeb1d2a5f4ca620dfe1c8deb62733f2d69a7f29
RoboRobert <nathan.estep@gmail.com>
2024-11-30 23:23:20 -0600

    adds 2024 scaffolding

--- y2023/src/day13/mod.rs
+++ y2023/src/day13/mod.rs
@@ -1,11 +1,29 @@
-use std::fs;
+fn transpose<T: Clone>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    if matrix.is_empty() {
+        return Vec::new();
+    }
+
+    // Determine the number of rows and columns
+    let num_rows = matrix.len();
+    let num_cols = matrix[0].len();
+
+    // Create a new 2D vector with dimensions num_cols x num_rows
+    let mut transposed = vec![vec![]; num_cols];
+
+    for col in 0..num_cols {
+        for row in 0..num_rows {
+            transposed[col].push(matrix[row][col].clone());
+        }
+    }
+
+    transposed
+}
 
 pub fn puzzle1(input: &str) -> i128 {
-    let contents = fs::read_to_string("example.txt").expect("File not opened properly!\n");
-
     //Gathers the different patterns into a vector
-    let pattern_vec: Vec<&str> = contents.split("\n\n").collect();
-    let mut pattern_vec_transpose: Vec<String> = Vec::new();
+    let pattern_vec: Vec<&str> = input.split("\n\n").collect();
+    
+    // let mut pattern_vec_transpose: Vec<>
 
     return 0;
 }
---
+++ y2024/src/day01/mod.rs
@@ -0,0 +1,8 @@
+pub fn puzzle1(input: &str) -> u32 {
+    54;
+}
+
+
+pub fn puzzle2(input: &str) -> u32 {
+    52;
+}
