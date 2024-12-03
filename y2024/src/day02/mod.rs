2ba8e08c9efdb0e1a2eaa5cc309c91483a239080
RoboRobert <nathan.estep@gmail.com>
2024-12-02 19:41:48 -0600

    puts safe checker into function

--- y2024/src/day02/mod.rs
+++ y2024/src/day02/mod.rs
@@ -1,16 +1,24 @@
+pub fn safe(input: Vec<i128>) -> bool {
+    let increasing = (input[0] < input[1]);
+    let max_diff: i128 = input.windows(2).map(|window| {window[1] - window[0]}).max().unwrap();
+    let min_diff: i128 = input.windows(2).map(|window| {window[1] - window[0]}).min().unwrap();
+
+    // All fail cases
+    if(std::cmp::max(min_diff.abs(), max_diff.abs()) > 3 || std::cmp::min(min_diff.abs(), max_diff.abs()) < 1
+    || (increasing && min_diff < 0) || (!increasing && max_diff > 0)) {
+        return false;
+    }
+
+    return true;
+}
+
 pub fn puzzle1(input: &str) -> i128 {
     let record_vec: Vec<Vec<i128>> = input.lines().map(|line| {line.split_whitespace().map(|current| {str::to_string(current).parse::<i128>().unwrap()}).collect()}).collect();
 
     let mut sum: i128 = 0;
 
     for ele in record_vec {
-        let increasing = (ele[0] < ele[1]);
-        let max_diff: i128 = ele.windows(2).map(|window| {window[1] - window[0]}).max().unwrap();
-        let min_diff: i128 = ele.windows(2).map(|window| {window[1] - window[0]}).min().unwrap();
-
-        // All fail cases
-        if(std::cmp::max(min_diff.abs(), max_diff.abs()) > 3 || std::cmp::min(min_diff.abs(), max_diff.abs()) < 1
-        || (increasing && min_diff < 0) || (!increasing && max_diff > 0)) {
+        if(!safe(ele)) {
             continue;
         }
 
@@ -28,15 +36,9 @@ pub fn puzzle2(input: &str) -> i128 {
 
     for ele in record_vec {
         for i in 0..ele.len() {
-            let mut newEle = ele.clone();
-            newEle.remove(i);
-            let increasing = (newEle[0] < newEle[1]);
-            let max_diff: i128 = newEle.windows(2).map(|window| {window[1] - window[0]}).max().unwrap();
-            let min_diff: i128 = newEle.windows(2).map(|window| {window[1] - window[0]}).min().unwrap();
-
-            // All fail cases
-            if(std::cmp::max(min_diff.abs(), max_diff.abs()) > 3 || std::cmp::min(min_diff.abs(), max_diff.abs()) < 1
-            || (increasing && min_diff < 0) || (!increasing && max_diff > 0)) {
+            let mut new_ele = ele.clone();
+            new_ele.remove(i);
+            if (!safe(new_ele)) {
                 continue;
             }
 
