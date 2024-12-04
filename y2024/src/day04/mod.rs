0de063d60478d1eaad227fadfae0ff8df55a91aa
RoboRobert <nathan.estep@gmail.com>
2024-12-03 22:39:58 -0600

    day 04 initialization

---
+++ y2024/src/day04/mod.rs
@@ -0,0 +1,13 @@
+pub fn puzzle1(input: &str) -> i128 {
+    let mut sum: i128 = 0;
+
+
+    return sum;
+}
+
+pub fn puzzle2(input: &str) -> i128 {
+    let mut sum: i128 = 0;
+
+
+    return sum;
+}
--- y2024/src/main.rs
+++ y2024/src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 use std::{env, fs};
 
@@ -16,6 +17,8 @@ fn main() {
         (2, 2) => day02::puzzle2(&input).to_string(),
         (3, 1) => day03::puzzle1(&input).to_string(),
         (3, 2) => day03::puzzle2(&input).to_string(),
+        (4, 1) => day04::puzzle1(&input).to_string(),
+        (4, 2) => day04::puzzle2(&input).to_string(),
 
         _ => panic!("no puzzle {} for day {}", puzzle, day),
     };
