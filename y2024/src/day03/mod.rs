3f4b4127ca0beb4c0bb615c4c88aceb10336cef1
RoboRobert <nathan.estep@gmail.com>
2024-12-03 22:56:27 -0600

    uses better rust regex syntax and adds tests

--- y2024/src/day03/mod.rs
+++ y2024/src/day03/mod.rs
@@ -1,35 +1,55 @@
 use regex::{self, Captures, Regex};
 
 pub fn puzzle1(input: &str) -> i128 {
-    let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
-    let matches: Vec<Captures> = re.captures_iter(input).collect();
-
-    return matches.iter().map(|capture| {
-        capture.get(0).unwrap().as_str()}).map(|string | {
-            let split: Vec<&str> = string.split(['(',',',')']).collect();
-            split[1].parse::<i128>().unwrap()
-            *split[2].parse::<i128>().unwrap()}).sum();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    return re.captures_iter(input).map(|capture| {capture[1].parse::<i128>().unwrap()*capture[2].parse::<i128>().unwrap()}).sum();
 }
 
 pub fn puzzle2(input: &str) -> i128 {
-    let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)").unwrap();
-    let matches: Vec<Captures> = re.captures_iter(input).collect();
-
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    
     let mut doBool: bool = true;
     let mut sum: i128 = 0;
-    for ele in matches {
-        let current: &str = ele.get(0).unwrap().as_str();
-        match(current) {
+    re.captures_iter(input).for_each(|f| {
+        match(&f[0]) {
             "do()" => {doBool = true;}
             "don't()" => {doBool = false;}
             _ => {
                 if(doBool) {
-                    let split: Vec<&str> = current.split(['(',',',')']).collect();
-                    sum += split[1].parse::<i128>().unwrap()*split[2].parse::<i128>().unwrap();
+                    sum += f[1].parse::<i128>().unwrap()*f[2].parse::<i128>().unwrap();
                 }
             }
         }
-    }
+    });
 
     return sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE1: &str = include_str!("example1.txt");
+    const EXAMPLE2: &str = include_str!("example2.txt");
+    const INPUT: &str = include_str!("input.txt");
+
+    #[test]
+    fn test_day_03_puzzle1_example() {
+        assert_eq!(puzzle1(EXAMPLE1), 161);
+    }
+
+    #[test]
+    fn test_day_03_puzzle1_input() {
+        assert_eq!(puzzle1(INPUT), 166630675);
+    }
+
+    #[test]
+    fn test_day_03_puzzle2_example() {
+        assert_eq!(puzzle2(EXAMPLE2), 48);
+    }
+
+    #[test]
+    fn test_day_03_puzzle2_input() {
+        assert_eq!(puzzle2(INPUT), 93465710);
+    }
 }
