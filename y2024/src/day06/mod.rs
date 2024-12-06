2148d9c40cb2929acf219f1b6815109c51b1e564
RoboRobert <nathan.estep@gmail.com>
2024-12-06 01:25:35 -0600

    must sleep, not done

--- y2024/src/day06/mod.rs
+++ y2024/src/day06/mod.rs
@@ -1,13 +1,71 @@
-#[derive(Debug, PartialEq)]
-struct Rule {
-    first: i128,
-    last: i128,
+trait Get<T> { 
+    fn get_checked(&self, index: isize) -> Option<T>;
+}
+
+impl<T> Get<T> for Vec<T> where T : Clone {
+    fn get_checked(&self, index: isize) -> Option<T> {
+        if(index < 0 || index >= self.len() as isize) {
+            return None;
+        }
+
+        return Some(self[index as usize].clone());
+    }
+}
+
+trait Get2D<T> { 
+    fn get_2d(&self, index: (isize, isize)) -> Option<T>;
+}
+
+impl<T> Get2D<T> for Vec<Vec<T>> where T : Clone {
+    fn get_2d(&self, index: (isize, isize)) -> Option<T> {
+        if(index.0 < 0 || index.1 < 0 || index.0 >= self[0].len() as isize || index.1 >= self.len() as isize) {
+            return None;
+        }
+    
+        return Some(self[index.0 as usize].get(index.1 as usize).unwrap().clone());
+    }
 }
 
 pub fn puzzle1(input: &str) -> i128 {
     let mut sum: i128 = 0;
 
-    
+    let mut start_pos: (isize, isize) = (0,0);
+
+    // let char_vec: Vec<Vec<char>> = input.lines().
+    // // Finds where to start
+    // for ele in input.lines().enumerate() {
+    //     let found = ele.1.find('^');
+    //     match found {
+    //         None => continue,
+    //         _ => {start_pos = (ele.0 as isize, found.unwrap() as isize); break;}
+    //     }
+    // }
+
+    // let direction =  (-1,0);
+    // let mut current_pos = Some(start_pos);
+    // while current_pos != None {
+    //     let forward = str_vec.get_2d
+    //     if(str_vec.get_checked(direction.0))
+    //     match direction {
+    //         // Up
+    //         (-1,0) => {
+                
+    //         },
+    //         // Down
+    //         (1,0) => {
+
+    //         },
+    //         // Left
+    //         (0,-1) => {
+
+    //         },
+    //         // Right
+    //         (0,1) => {
+
+    //         },
+    //         _ => print!("WEIRD"),
+    //     }
+    // }
 
     return sum;
 }
