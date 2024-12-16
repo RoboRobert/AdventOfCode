c8c5c630f906a509ad8e20c487f7bd11c40d3c84
Nathan Estep <nathan.estep@gmail.com>
2024-12-16 13:55:59 -0500

    refactors part 1 to stop using binary counting

--- y2024/src/day07/mod.rs
+++ y2024/src/day07/mod.rs
@@ -7,13 +7,14 @@ struct Equation {
     operands: Vec<i128>,
 }
 
-// Returns whether the given ops work
-fn do_ops(eq: Equation, ops: String) -> bool {
+// Returns whether the given ops work for part 1
+fn do_ops(eq: Equation, ops: Vec<u32>) -> bool {
     let mut value = eq.operands[0];
     for i in 1..eq.operands.len() {
-        match(ops.chars().nth(i-1).unwrap()) {
-            '+' => value = value + eq.operands[i],
-            '*' => value = value * eq.operands[i],
+        match(ops[i-1]) {
+            1 => value = value + eq.operands[i],
+            2 => value = value * eq.operands[i],
+            3 => value = value * eq.operands[i],
             _ => {dbg!("WEIRD");}
         }
     }
@@ -40,29 +41,12 @@ pub fn puzzle1(input: &str) -> i128 {
         .collect();
 
     for ele in equation_vec {
-        let base: u32 = 2;
         let num_ops:u32 = ele.operands.len() as u32 -1;
-
-        let mut value: u16 = 0b0000000;
-        for i in 0..base.pow(num_ops) {
-            let s_str: String = format!("{value:016b}");
-            let mut ops_str: String = "".to_string();
-
-            let start_i = (16-num_ops) as usize;
-            for i in start_i..s_str.len() {
-                match(s_str.chars().nth(i).unwrap()) {
-                    '0' => ops_str += "+",
-                    '1' => ops_str += "*",
-                    _ =>  {dbg!("WEIRD");}
-                }
-            }
-
-            if(do_ops(ele.clone(), ops_str)) {
+        for perm in (1 as u32..=num_ops).map(|_| 1 as u32..=2 as u32).multi_cartesian_product() {
+            if(do_ops(ele.clone(), perm)) {
                 sum += ele.result;
                 break;
             }
-            
-            value += 1;
         }
     }
 
@@ -71,6 +55,27 @@ pub fn puzzle1(input: &str) -> i128 {
 
 pub fn puzzle2(input: &str) -> i128 {
     let mut sum: i128 = 0;
+
+    let mut equation_vec: Vec<Equation> = input
+        .lines()
+        .map(|line| {
+            let mut split = line.split([':', ' ']);
+            Equation {
+                result: split.nth(0).unwrap().parse::<i128>().unwrap(),
+                operands: split.skip(1).map(|e| e.parse::<i128>().unwrap()).collect(),
+            }
+        })
+        .collect();
+
+    for ele in equation_vec {
+        let num_ops:u32 = ele.operands.len() as u32 -1;
+        for perm in (1 as u32..=num_ops).map(|_| 1 as u32..=2 as u32).multi_cartesian_product() {
+            if(do_ops(ele.clone(), perm)) {
+                sum += ele.result;
+                break;
+            }
+        }
+    }
 
     return sum;
 }
@@ -84,17 +89,17 @@ mod tests {
 
     #[test]
     fn test_day_07_puzzle1_example() {
-        assert_eq!(puzzle1(EXAMPLE), 143);
+        assert_eq!(puzzle1(EXAMPLE), 3748);
     }
 
     #[test]
     fn test_day_07_puzzle1_input() {
-        assert_eq!(puzzle1(INPUT), 6034);
+        assert_eq!(puzzle1(INPUT), 1399219271639);
     }
 
     #[test]
     fn test_day_07_puzzle2_example() {
-        assert_eq!(puzzle2(EXAMPLE), 123);
+        assert_eq!(puzzle2(EXAMPLE), 11387);
     }
 
     #[test]
