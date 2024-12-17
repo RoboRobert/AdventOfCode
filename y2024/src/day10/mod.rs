852fdd76772063cd49cdacc1052fe08c89c838eb
Nathan Estep <nathan.estep@gmail.com>
2024-12-16 22:25:35 -0500

    starts day 10 (looks like a BFS to me boys)

--- y2024/src/day10/mod.rs
+++ y2024/src/day10/mod.rs
@@ -1,4 +1,56 @@
+trait Access2D<T> {
+    fn get_2d(&self, index: (isize, isize)) -> Option<&T>;
+}
 
+impl<T> Access2D<T> for Vec<Vec<T>> {
+    fn get_2d(&self, index: (isize, isize)) -> Option<&T> {
+        if (index.0 < 0
+            || index.1 < 0
+            || index.0 >= self[0].len() as isize
+            || index.1 >= self.len() as isize)
+        {
+            return None;
+        }
+
+        return self[index.0 as usize].get(index.1 as usize);
+    }
+}
+
+// Does a search from the starting trailhead and returns the score
+pub fn search(char_vec: &Vec<&str>, start: (isize,isize), bounds: (usize, usize)) -> i128 {
+    let mut num_xmases = 0;
+    
+    let mut moves_vec: Vec<Option<Move>> = Vec::new();
+    
+    moves_vec.push(Some(Move { x: start.0, y: start.1, word:"X".to_string() }));
+
+    while moves_vec.len() > 0 {
+        let current = moves_vec.pop().unwrap();
+        match current {
+            None => continue,
+            _ => {}
+        }
+
+        let new_current = current.unwrap();
+        let current_word = new_current.word;
+        if(current_word == "XMAS") {
+            num_xmases += 1;
+        }
+        let x = new_current.x;
+        let y = new_current.y;
+        
+        moves_vec.push(get_move(char_vec, current_word.clone(), x-1, y-1, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x, y-1, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x+1, y-1, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x-1, y, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x+1, y, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x-1, y+1, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x, y+1, bounds));
+        moves_vec.push(get_move(char_vec, current_word.clone(), x+1, y+1, bounds));
+    }
+
+    return num_xmases;
+}
 
 pub fn puzzle1(input: &str) -> i128 {
     let mut sum: i128 = 0;
