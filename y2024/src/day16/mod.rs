2d3b61ca629b24459df4665545d58e9dd4350159
Nathan Estep <nathan.estep@gmail.com>
2024-12-19 15:13:25 -0500

    part 2 giving more trouble than expected

--- y2024/src/day16/mod.rs
+++ y2024/src/day16/mod.rs
@@ -1,10 +1,8 @@
-use std::{cmp::{Ordering, Reverse}, collections::{BinaryHeap, HashMap}, hash::Hash};
-
-use itertools::Itertools;
-
-#[derive(Debug, Eq, Clone, Copy)]
+use std::{cmp::Ordering, collections::{HashMap, HashSet}};
+#[derive(Debug, Eq, Clone)]
 struct Node {
     pos: (isize, isize),
+    prevs: HashSet<(isize, isize)>,
     dir: (isize, isize),
     weight: i128,
 }
@@ -27,32 +25,36 @@ impl Ord for Node {
     }
 }
 
-// Returns the minimum score to reach the end node from the start node
-fn min_score(weight_map: &HashMap<(isize, isize), Node>, start: (isize, isize), end: (isize, isize)) -> i128 {
+// Returns a hashmap containing the minimum path distances for every node, as well as the previous nodes for each node
+// Assumes the start node is initialized with weight 0
+fn min_map(weight_map: &HashMap<(isize, isize), Node>) -> HashMap<(isize, isize), Node> {
     let mut map_mut: HashMap<(isize, isize), Node> = weight_map.clone();
     let mut seen_map: HashMap<(isize, isize), Node> = HashMap::new();
 
     // Loop until all nodes are visited
     while(map_mut.len() > 0) {
         // Pops the unvisited node with lowest weight
-        let current = *map_mut.values().min().unwrap();
+        let current = map_mut.values().min().unwrap().clone();
         let pos = current.pos;
+
+        // Moves current node from visited to unvisited
         map_mut.remove(&pos);
-        seen_map.insert(pos, current);
+        seen_map.insert(pos, current.clone());
 
         // Direction vector with up, down, left, right
         let dir_vec: Vec<(isize, isize)> = vec![(-1, 0),(1,0),(0,-1), (0,1)];
-        // Goes through all neighbors of the current node
+        // Goes through all neighbors of the current node using the dir_vec
         for new_dir in dir_vec {
             let n_pos = (pos.0 + new_dir.0, pos.1 + new_dir.1);
             let neighbor = map_mut.get(&n_pos);
             match neighbor {
+                // If the neighbor isn't in the graph, do nothing
                 None => continue,
                 _ => {},
             }
-
-            let mut n_update = Node{pos: n_pos, dir: new_dir, weight: 0};
+            
             let n_unwrap = neighbor.unwrap();
+            let mut n_update = Node{pos: n_pos, prevs: current.prevs.clone(), dir: new_dir, weight: 0};
 
             if(new_dir != current.dir) {
                 n_update.weight += 1001;
@@ -62,70 +64,66 @@ fn min_score(weight_map: &HashMap<(isize, isize), Node>, start: (isize, isize), 
             }
             n_update.weight += current.weight;
 
-            // If the new weight is lower, replace in the unvisited list
-            if(n_update.weight < n_unwrap.weight) {
+            // If the new weight is the same or lower, replace its weight in the unvisited list
+            if(n_update.weight <= n_unwrap.weight) {
+                n_update.prevs.insert(current.pos);
                 map_mut.insert(n_update.pos, n_update);
             }
         }
     }
 
-    return seen_map.get((&end)).unwrap().weight;
+    return seen_map;
+}
+
+// Creates a map of Nodes from a 2D Vector of chars and returns start and end positions
+fn create_map(char_vec: Vec<Vec<char>>) -> (HashMap<(isize, isize), Node>,(isize, isize),(isize, isize)) {
+    let mut weight_map: HashMap<(isize, isize), Node> = HashMap::new();
+    let mut start: (isize, isize) = (0,0);
+    let mut end: (isize, isize) = (0,0);
+    for row in char_vec.iter().enumerate() {
+        for col in row.1.iter().enumerate() {
+            let pos = (row.0 as isize, col.0 as isize);
+            let mut node: Node = Node{pos: (pos), prevs: HashSet::new(), dir: (0,0), weight: 0};
+
+            // Insert everything except the walls
+            match *col.1 {
+                // Start faces East (0,1)
+                'S' => {node = Node{pos: (pos), prevs: HashSet::new(), dir: (0,1), weight: 0}; start = pos;}
+                'E' => {node = Node{pos: (pos), prevs: HashSet::new(), dir: (0,0), weight: i128::MAX}; end = pos;}
+                '.' => node = Node{pos: (pos), prevs: HashSet::new(), dir: (0,0), weight: i128::MAX},
+                '#' => continue,
+                _ => {dbg!("WEIRD");}
+            }
+            weight_map.insert(pos, node);
+        }
+    }
+
+    return (weight_map, start, end);
 }
 
 pub fn puzzle1(input: &str) -> i128 {
     let char_vec: Vec<Vec<char>> = input.split("\n\n").nth(0).unwrap().lines().map(|line| line.chars().collect()).collect();
 
-    let mut weight_map: HashMap<(isize, isize), Node> = HashMap::new();
-    let mut start: (isize, isize) = (0,0);
-    let mut end: (isize, isize) = (0,0);
-    for row in char_vec.iter().enumerate() {
-        for col in row.1.iter().enumerate() {
-            let pos = (row.0 as isize, col.0 as isize);
-            let mut node: Node = Node{pos: (pos), dir: (0,0), weight: 0};
+    let map_tuple = create_map(char_vec);
+    let weight_map = map_tuple.0;
+    let end = map_tuple.2;
 
-            // Insert everything except the walls
-            match *col.1 {
-                // Start faces East (0,1)
-                'S' => {node = Node{pos: (pos), dir: (0,1), weight: 0}; start = pos;}
-                'E' => {node = Node{pos: (pos), dir: (0,0), weight: i128::MAX}; end = pos;}
-                '.' => node = Node{pos: (pos), dir: (0,0), weight: i128::MAX},
-                '#' => continue,
-                _ => {dbg!("WEIRD");}
-            }
-            weight_map.insert(pos, node);
-        }
-    }
-
-    return min_score(&mut weight_map, start, end);
+    return min_map(&weight_map).get(&end).unwrap().weight;
 }
 
 pub fn puzzle2(input: &str) -> i128 {
-    let mut sum: i128 = 0;
-
     let char_vec: Vec<Vec<char>> = input.split("\n\n").nth(0).unwrap().lines().map(|line| line.chars().collect()).collect();
 
-    let mut weight_map: HashMap<(isize, isize), Node> = HashMap::new();
-    let mut start: (isize, isize) = (0,0);
-    let mut end: (isize, isize) = (0,0);
-    for row in char_vec.iter().enumerate() {
-        for col in row.1.iter().enumerate() {
-            let pos = (row.0 as isize, col.0 as isize);
-            let mut node: Node = Node{pos: (pos), dir: (0,0), weight: 0};
+    let map_tuple = create_map(char_vec);
+    let weight_map = map_tuple.0;
+    let end = map_tuple.2;
 
-            // Insert everything except the walls
-            match *col.1 {
-                // Start faces East (0,1)
-                'S' => {node = Node{pos: (pos), dir: (0,1), weight: 0}; start = pos;}
-                'E' => {node = Node{pos: (pos), dir: (0,0), weight: i128::MAX}; end = pos;}
-                '.' => node = Node{pos: (pos), dir: (0,0), weight: i128::MAX},
-                '#' => continue,
-                _ => {dbg!("WEIRD");}
-            }
-            weight_map.insert(pos, node);
-        }
-    }
+    let new_map = min_map(&weight_map);
+    let prevs = new_map.get(&end).unwrap().prevs.clone();
+    
+    dbg!(&prevs);
 
-    return min_score(&mut weight_map, start, end);
+    return prevs.len() as i128;
 }
 
 #[cfg(test)]
