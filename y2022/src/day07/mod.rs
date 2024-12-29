2715717be589af9282e6a94938eca5d4d1d6497a
Nathan Estep <nathan.estep@gmail.com>
2024-12-28 22:20:30 -0500

    fixed by making paths unique

--- y2022/src/day07/mod.rs
+++ y2022/src/day07/mod.rs
@@ -1,44 +1,42 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, thread::current};
 
-fn add_size(size: i128, dir: &str, map: &mut HashMap<&str, Dir>) {
-    let mut current = dir;
+fn add_size(size: i128, path: &str, map: &mut HashMap<String, i128>) {
+    let mut current = path;
     while current != "" {
-        let current_dir = map.get_mut(current).unwrap();
-        current_dir.size += size;
-        current = current_dir.parent;
+        *map.entry(current.to_owned()).or_insert(0) += size;
+        current = &current[0..current.rfind('/').unwrap_or(0)];
     }
 }
 
-fn get_directories(input:&str) -> HashMap<&str, i128> {
-    let mut dir_map: HashMap<&str, i128> = HashMap::new();
-
-    dir_map.insert("/", Dir {name: "/", parent: "", size: 0 });
+fn get_directories(input:&str) -> HashMap<String, i128> {
+    let mut dir_map: HashMap<String, i128> = HashMap::new();
     
-    let mut current = Dir {name: "", parent: "", size: 0 };
+    let mut current_path = String::from("/");
     for ele in input.lines().enumerate() {
-        let name = current.name;
-        println!("{name}");
+        if(ele.1 == "$ cd /") {
+            continue;
+        }
+        println!("{current_path}");
         let mut split = ele.1.split(" ");
         let first = split.nth(0).unwrap();
         let second = split.nth(0).unwrap();
         let third = split.nth(0);
         // Handle adding file sizes
         if (first.parse::<i128>().is_ok()) {
-            add_size(first.parse::<i128>().unwrap(), current.name, &mut dir_map);
+            add_size(first.parse::<i128>().unwrap(), &current_path, &mut dir_map);
         }
         // Handle directory change
         else if(second == "cd") {
             let dir = third.unwrap();
-            let current_parent = current.parent;
             // dbg!(current_parent);
             match dir {
                 ".." => {
-                    if(dir_map.contains_key(current_parent)) {
-                        current = dir_map.get(&current_parent).unwrap().clone();
-                    }
+                    current_path = current_path[0..current_path.rfind('/').unwrap()].to_string();
                 },
                 _ => {
-                    current = *dir_map.entry(dir).or_insert(Dir { parent: current.name, name:dir, size: 0 });
+                    let mut add = String::from("/");
+                    add += dir;
+                    current_path += add.as_str();
                 },
             }
         }
@@ -51,22 +49,21 @@ fn get_directories(input:&str) -> HashMap<&str, i128> {
 pub fn puzzle1(input: &str) -> i128 {
     let dir_map = get_directories(input);
 
-    // for ele in &dir_map {
-    //     dbg!(ele.1);
-    // }
-
-    dir_map.values().filter(|val| val.size <= 100000).map(|val| val.size).sum()
+    dir_map.values().filter(|&val| val <= &100000).sum()
 }
 
 pub fn puzzle2(input: &str) -> i128 {
     let dir_map = get_directories(input);
 
-    let size_needed: i128 = 30000000;
-    let current_size: i128 = dir_map.get("/").unwrap().size;
+    let total_size: i128 = 70000000;
+    let space_needed: i128 = 30000000;
+    let current_space: i128 = total_size - *dir_map.get("/").unwrap();
 
-    let min_diff = current_size - size_needed;
+    dbg!(current_space);
 
-    dir_map.values().filter(|val| val.size >= min_diff).map(|val| val.size).min().unwrap()
+    let min_diff = space_needed - current_space;
+
+    *dir_map.values().filter(|&val| *val > min_diff).min().unwrap()
 }
 
 #[cfg(test)]
