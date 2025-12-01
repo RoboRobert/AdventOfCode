ab14bd6c63198386461d8a5788b2b1e8a91f20a9
RoboRobert <nathan.estep@gmail.com>
2025-11-30 19:14:16 -0500

    starts day 01

--- y2025/src/day01/mod.rs
+++ y2025/src/day01/mod.rs
@@ -1,67 +1,7 @@
 pub fn puzzle1(input: &str) -> u32 {
-    let mut left: Vec<u32> = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .nth(0)
-                .unwrap()
-                .parse::<u32>()
-                .unwrap()
-        })
-        .collect();
-    let mut right: Vec<u32> = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .nth(1)
-                .unwrap()
-                .parse::<u32>()
-                .unwrap()
-        })
-        .collect();
-
-    left.sort();
-    right.sort();
-
-    let mut sum: u32 = 0;
-
-    for i in 0..left.len() {
-        sum += left[i].abs_diff(right[i])
-    }
-
-    return sum;
+    0
 }
 
 pub fn puzzle2(input: &str) -> u64 {
-    let mut left: Vec<u64> = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .nth(0)
-                .unwrap()
-                .parse::<u64>()
-                .unwrap()
-        })
-        .collect();
-    let mut right: Vec<u64> = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .nth(1)
-                .unwrap()
-                .parse::<u64>()
-                .unwrap()
-        })
-        .collect();
-
-    left.sort();
-    right.sort();
-
-    let mut sum: u64 = 0;
-
-    for ele in left {
-        sum += (right.iter().filter(|&element| *element == ele).count() as u64) * ele;
-    }
-
-    return sum;
+    0
 }
