943be9dcaf13793a7ea707bab29c92b5b8d01b66
RoboRobert <nathan.estep@gmail.com>
2025-12-03 21:15:16 -0500

    finish day 03

--- y2025/src/day03/mod.rs
+++ y2025/src/day03/mod.rs
@@ -1,3 +1,5 @@
+use std::convert;
+
 pub fn puzzle1(input: &str) -> i64 {
     let mut sum: i64 = 0;
 
@@ -38,8 +40,46 @@ pub fn puzzle1(input: &str) -> i64 {
     sum
 }
 
+fn convert_vec(input: &Vec<(usize, i64)>) -> i64 {
+    let mut result = 0;
+    input.iter().for_each(|item| {
+        result *= 10;
+        result += item.1;
+    });
+
+    return result;
+}
+
 pub fn puzzle2(input: &str) -> i64 {
-    let sum: i64 = 0;
+    let mut sum: i64 = 0;
+
+    input.lines().for_each(|line| {
+        let mut biggest_pos_vec: Vec<(usize, i64)> = vec![];
+
+        let battery_vec: Vec<i64> = line
+            .chars()
+            .map(|char| char.to_digit(10).unwrap().try_into().unwrap())
+            .collect();
+
+        for _ in 0..12 {
+            let mut temp_biggest_vec: Vec<(usize, i64)> = biggest_pos_vec.clone();
+
+            for (position, battery) in battery_vec.iter().enumerate() {
+                if !biggest_pos_vec.contains(&(position, *battery)) {
+                    let mut test_biggest_vec: Vec<(usize, i64)> = biggest_pos_vec.clone();
+                    test_biggest_vec.push((position, *battery));
+                    test_biggest_vec.sort_by(|a, b| a.0.cmp(&b.0));
+                    if convert_vec(&test_biggest_vec) > convert_vec(&temp_biggest_vec) {
+                        temp_biggest_vec = test_biggest_vec.clone();
+                    }
+                }
+            }
+
+            biggest_pos_vec = temp_biggest_vec;
+        }
+
+        sum += convert_vec(&biggest_pos_vec);
+    });
 
     sum
 }
@@ -58,16 +98,16 @@ mod tests {
 
     #[test]
     fn test_day_03_puzzle1_input() {
-        assert_eq!(puzzle1(INPUT), 31839939622);
+        assert_eq!(puzzle1(INPUT), 17316);
     }
 
     #[test]
     fn test_day_03_puzzle2_example() {
-        assert_eq!(puzzle2(EXAMPLE), 4174379265);
+        assert_eq!(puzzle2(EXAMPLE), 3121910778619);
     }
 
     #[test]
     fn test_day_03_puzzle2_input() {
-        assert_eq!(puzzle2(INPUT), 41662374059);
+        assert_eq!(puzzle2(INPUT), 171741365473332);
     }
 }
