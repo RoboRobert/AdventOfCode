2aa43acf0d55308e1f4ba6462950b6e22f2de1f6
RoboRobert <nathan.estep@gmail.com>
2025-12-07 14:50:42 -0500

    finish part 2

--- y2025/src/day07/mod.rs
+++ y2025/src/day07/mod.rs
@@ -1,7 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
-pub fn puzzle1(input: &str) -> i64 {
-    let mut sum: i64 = 0;
+fn do_puzzle(input: &str) -> (i64, i64) {
+    let mut possible_paths: i64 = 0;
+    let mut num_splits: i64 = 0;
 
     let input_char_vec: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
@@ -18,11 +19,15 @@ pub fn puzzle1(input: &str) -> i64 {
         }
     }
 
-    let mut tracked_beams: HashSet<(isize, isize)> = HashSet::from([(start.0 + 1, start.1)]);
+    let mut tracked_beams: HashMap<(isize, isize), i64> = HashMap::new();
+
+    tracked_beams.insert((start.0 + 1, start.1), 1);
 
     loop {
-        let mut new_beams: HashSet<(isize, isize)> = HashSet::new();
-        for beam in &tracked_beams {
+        let mut new_beams: HashMap<(isize, isize), i64> = HashMap::new();
+        for beam_element in &tracked_beams {
+            let beam = beam_element.0;
+            let current_value = beam_element.1;
             let unwrapped_state = manifold_map.get(beam);
             if unwrapped_state.is_none() {
                 continue;
@@ -30,13 +35,21 @@ pub fn puzzle1(input: &str) -> i64 {
 
             let current_state = unwrapped_state.unwrap();
             if current_state == &'^' {
-                sum += 1;
-                new_beams.insert((beam.0, beam.1 - 1));
-                new_beams.insert((beam.0, beam.1 + 1));
-            }
-            if current_state == &'.' {
+                num_splits += 1;
+                new_beams
+                    .entry((beam.0 + 1, beam.1 - 1))
+                    .and_modify(|value| *value += current_value)
+                    .or_insert(*current_value);
+                new_beams
+                    .entry((beam.0 + 1, beam.1 + 1))
+                    .and_modify(|value| *value += current_value)
+                    .or_insert(*current_value);
+            } else if current_state == &'.' {
                 manifold_map.insert(*beam, '|');
-                new_beams.insert((beam.0 + 1, beam.1));
+                new_beams
+                    .entry((beam.0 + 1, beam.1))
+                    .and_modify(|value| *value += current_value)
+                    .or_insert(*current_value);
             }
         }
         if new_beams.len() == 0 {
@@ -46,67 +59,19 @@ pub fn puzzle1(input: &str) -> i64 {
         tracked_beams = new_beams.clone();
     }
 
-    sum
+    for beam in tracked_beams {
+        possible_paths += beam.1;
+    }
+
+    (num_splits, possible_paths)
+}
+
+pub fn puzzle1(input: &str) -> i64 {
+    do_puzzle(input).0
 }
 
 pub fn puzzle2(input: &str) -> i64 {
-    let mut sum: i64 = 0;
-
-    let input_char_vec: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-
-    let height = input_char_vec.len();
-    let width = input_char_vec[0].len();
-
-    let mut manifold_map: HashMap<(isize, isize), char> = HashMap::new();
-
-    let mut start: (isize, isize) = (0, 0);
-
-    for (i, char_vec) in input_char_vec.iter().enumerate() {
-        for (j, character) in char_vec.iter().enumerate() {
-            if character == &'S' {
-                start = (i as isize, j as isize);
-            }
-            manifold_map.insert((i as isize, j as isize), *character);
-        }
-    }
-
-    let mut tracked_beams: HashSet<(isize, isize)> = HashSet::from([(start.0 + 1, start.1)]);
-
-    loop {
-        let mut new_beams: HashSet<(isize, isize)> = HashSet::new();
-        for beam in &tracked_beams {
-            let unwrapped_state = manifold_map.get(beam);
-            if unwrapped_state.is_none() {
-                continue;
-            }
-
-            let current_state = unwrapped_state.unwrap();
-            if current_state == &'^' {
-                sum += 1;
-                new_beams.insert((beam.0, beam.1 - 1));
-                new_beams.insert((beam.0, beam.1 + 1));
-            }
-            if current_state == &'.' {
-                manifold_map.insert(*beam, '|');
-                new_beams.insert((beam.0 + 1, beam.1));
-            }
-        }
-        if new_beams.len() == 0 {
-            break;
-        }
-
-        tracked_beams = new_beams.clone();
-
-        for i in 0..height {
-            for j in 0..width {
-                print!("{}", manifold_map.get(&(i as isize, j as isize)).unwrap());
-            }
-            println!();
-        }
-        println!();
-    }
-
-    sum
+    do_puzzle(input).1
 }
 
 #[cfg(test)]
